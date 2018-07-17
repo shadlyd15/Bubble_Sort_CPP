8831e13d82d9990bce59f66026b034a4c8f4bd20
shadlyd15 <shadlyd15@gmail.com>
2018-07-18 00:46:59 +0600

    Code Added

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-uint8_t array[] = {2, 6, 1, 5, 8, 10, 2, 6, 1, 5, 8, 10, 2, 6, 1, 5, 8, 10, 2, 6, 1, 5, 8, 10, 2, 6, 1, 5, 8, 10, 2, 6, 1, 5, 8, 10};
+uint8_t array[] = {2, 6, 1, 5, 8, 10, 2, 6, 1, 5, 8, 10, 2, 6, 1, 5, 8, 10, 2};
 int itr = 0;
 void bubble_sort(uint8_t * array, size_t size){
 	bool swapped = false;
